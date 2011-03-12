effc76e4e1b40e67c6666664925a00a0374a2164
Melissa Filan <melissa.filan@gmail.com>
2011-03-12 16:07:26 -0500

    changes, not finished yet

--- index.php
+++ index.php
@@ -5,12 +5,19 @@ require_once('classes/tasks.php');
 
 $project = new MysqlDB();
 
-//$results=$arc_task->get($insertdata);
+$results=$project->get('tasks');
+print_r($results);
+
 //$insertData = array(
-//    'task_name' => 'task one',
-//    'project_id' => '3'
+//    'task_name' => 'task two',
+//    'project_id' => '2'
 //);
-$arc_task = $project->archived_tasks('tasks');
+$tasks = new tasks();
+$project->create_tasks('tasks',$insertData);
+
+
+$tasks = new tasks();
+$arc_task = $tasks->archived_tasks('tasks');
 print_r($arc_task);
 ?>
 
@@ -22,8 +29,13 @@ print_r($arc_task);
    <title>untitled</title>
 </head>
 <body>
+    <br />
+    <hr />
+    <form>
+        Task Name: <input type="text" name="task_name" value="<?php  $insertData[$title]; ?>" /><br />
+        Project ID: <input type="text" name="project_id" value="<?php  $project_id; ?>" />
+    </form>
 <pre>
-<?php  ; ?>
 </pre>
 </body>
-</html>;
+</html>
