c0f3944a0c01539b9b017cb87030595e4be1a43e
Melissa Filan <melissa.filan@gmail.com>
2011-03-12 13:19:53 -0500

    first commit, code still infantile

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <?php
 
-require_once('MySqlDb.php');
-$Db = new MySqlDb('localhost', 'root', 'root', 'db');
+require_once('classes/MySqlDb.php');
+require_once('classes/tasks.php');
 
 
+$Db = new MySqlDb('localhost', 'melissaf', 'password', 'taskerdb');
 
+$Db->$insertdata('tasks',
+    array('',
+        'firsttask',
+        '2',
+        '1',
+        '2'
+        ));
 
 
 ?>
+
+
 <!DOCTYPE html>
 
 <html lang="en">
 <head>
-   <meta charset="utf-8">
    <title>untitled</title>
 </head>
 <body>
 <pre>
-
+<?php $insertData ?>
 </pre>
 </body>
 </html>
