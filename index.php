4953e0ec7ad3c0fef0fc9f49ba43a6bd0aae593b
Tedi Konda <tedi@tkdesigns.tv>
2011-03-17 19:13:24 -0400

    made changes to index file

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 <?php
-<<<<<<< HEAD
-
-require_once('classes/MySqlDb.php');
-//$Db = new MySqlDb('localhost', 'tcass84', '985300', 'taskerdb');
-=======
 include "header.php"; 
 ?>
 <div id="dashboard">
 <img src="images/dashboard.png">
->>>>>>> design
 
-require_once('classes/Admin.php');
 
-<<<<<<< HEAD
-$status = new Admin('localhost', 'tcass84', '985300', 'taskerdb');
-
-$status_result = $status->get_status(3);
-=======
 <div class="dashboard_project">
 <h2 class="project_name"><a href="#" class="drop" title="Go to projects/tasks page">Project name</a><span class="edit"><a href="#" class="drop" title="Edit project"><img src="images/edit.png"></a></span></div>
->>>>>>> design
 
 <div class="dashboard_project">
 <h2 class="project_name"><a href="#" class="drop" title="Go to projects/tasks page">Other project</a><span class="edit"><a href="#" class="drop" title="Edit project"><img src="images/edit.png"></a></span></div>
@@ -28,18 +15,6 @@ $status_result = $status->get_status(3);
 <div class="dashboard_project">
 <h2 class="project_name"><a href="#" class="drop" title="Go to projects/tasks page">And another</a><span class="edit"><a href="#" class="drop" title="Edit project"><img src="images/edit.png"></a></span></div>
 
-<<<<<<< HEAD
-<html lang="en">
-<head>
-   <meta charset="utf-8">
-   <title>untitled</title>
-</head>
-<body>
-    <?php print_r($status_result) ?>
-</body>
-</html>
-=======
 </div>
 
 <?php include "footer.php"; ?>
->>>>>>> design
