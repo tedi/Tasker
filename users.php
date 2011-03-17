4ef3085badfeda4caaeaa4bea344dd748c826e9b
Tedi Konda <tedi@tkdesigns.tv>
2011-03-17 18:48:32 -0400

    add/edit users pages complete

---
+++ users.php
@@ -0,0 +1,31 @@
+<?php
+include "header.php"; 
+?>
+
+<div id="dashboard">
+<!-- <img src="images/user.png"> <a class="projectbutton" href="newuser.php">Add user</a> -->
+<div class="single_project">
+<h2 class="project_name"><a href="#">User role 1</a><span><a class="button" href="newuser.php">Add user</a></span></h2>
+<ul class="projects">
+<li><a href="#" class="task">Bree</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">David</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Melisa</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Thomas</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Tedi</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+</ul>
+</div>
+
+<div class="single_project">
+<h2 class="project_name"><a href="#" class="drop" title="Description of project name goes here">User role 2</a><span><a class="button" href="newuser.php">Add user</a></span></h2>
+<ul class="projects">
+<li><a href="#" class="task">Bree</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">David</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Melisa</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Thomas</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Tedi</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+</ul>
+</div>
+
+</div> <!-- End dashboard -->
+
+<?php include "footer.php"; ?>
