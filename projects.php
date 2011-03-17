091605cbc76a95ad2fafabcbe6b0c08c5dec0a68
Tedi Konda <tedi@tkdesigns.tv>
2011-03-17 18:55:24 -0400

    updated edit links

--- projects.php
+++ projects.php
@@ -7,20 +7,20 @@ include "header.php";
 <div class="single_project">
 <h2 class="project_name"><a href="#" class="drop" title="Description of project name goes here">Project name</a><span><a class="button" href="newtask.php">Add task</a></span></h2>
 <ul class="projects">
-<li><a href="#" class="task">Task 1</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="task">Second task</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="task">Third task</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="task">And another one</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Task 1</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Second task</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Third task</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">And another one</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
 </ul>
 </div>
 
 <div class="single_project">
 <h2 class="project_name"><a href="#" class="drop" title="Description of project name goes here">Test project</a><span><a class="button" href="newtask.php">Add task</a></span></h2>
 <ul class="projects">
-<li><a href="#" class="task">Task</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="task">Finish Tasker design</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="task">Debug code</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="task">Launch tasker</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Task</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Finish Tasker design</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Debug code</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="task">Launch tasker</a> <span class="del"><a href="#"class="drop" title="Delete task"><img src="images/del.png"></a></span> <span class="edit"><a href="edittask.php" class="drop" title="Edit task"><img src="images/edit.png"></a></span></li>
 </ul>
 </div>
 
--- users.php
+++ users.php
@@ -7,22 +7,22 @@ include "header.php";
 <div class="single_project">
 <h2 class="project_name"><a href="#">User role 1</a><span><a class="button" href="newuser.php">Add user</a></span></h2>
 <ul class="projects">
-<li><a href="#" class="user">Bree</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">David</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">Melisa</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">Thomas</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">Tedi</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Bree</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">David</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Melisa</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Thomas</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Tedi</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
 </ul>
 </div>
 
 <div class="single_project">
 <h2 class="project_name"><a href="#" class="drop" title="Description of project name goes here">User role 2</a><span><a class="button" href="newuser.php">Add user</a></span></h2>
 <ul class="projects">
-<li><a href="#" class="user">Bree</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">David</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">Melisa</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">Thomas</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
-<li><a href="#" class="user">Tedi</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="#" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Bree</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">David</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Melisa</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Thomas</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
+<li><a href="#" class="user">Tedi</a> <span class="del"><a href="#"class="drop" title="Delete user"><img src="images/del.png"></a></span> <span class="edit"><a href="edituser.php" class="drop" title="Edit user"><img src="images/edit.png"></a></span></li>
 </ul>
 </div>
 
