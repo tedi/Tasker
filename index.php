780db51c17ae7defcdeaeb7210f393f607827d1e
Tedi Konda <tedi@tkdesigns.tv>
2011-03-17 18:32:35 -0400

    added dashboard page

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 include "header.php"; 
 ?>
 <div id="dashboard">
+<img src="images/dashboard.png">
 
 
+<div class="dashboard_project">
+<h2 class="project_name"><a href="#" class="drop" title="Go to projects/tasks page">Project name</a><span class="edit"><a href="#" class="drop" title="Edit project"><img src="images/edit.png"></a></span></div>
+
+<div class="dashboard_project">
+<h2 class="project_name"><a href="#" class="drop" title="Go to projects/tasks page">Other project</a><span class="edit"><a href="#" class="drop" title="Edit project"><img src="images/edit.png"></a></span></div>
+
+
+<div class="dashboard_project">
+<h2 class="project_name"><a href="#" class="drop" title="Go to projects/tasks page">And another</a><span class="edit"><a href="#" class="drop" title="Edit project"><img src="images/edit.png"></a></span></div>
 
 </div>
 
